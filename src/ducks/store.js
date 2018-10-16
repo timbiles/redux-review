3a5ba1b4acf8ee9822d33249768ee562c49d2828
Tim Biles <timbilestimbiles@gmail.com>
2018-10-16 10:05:30 -0500

    fix edit

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,19 +1,32 @@
+///////////EXAMPLE OF COMBINED REDUCERS //////////////
+
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { promiseMiddleWare } from 'react-promise-middleware'
+
+// //bring in the reducer(s)
+// import reducer from './reducer';
+
+// const combined = combineReducers({
+//     userReducer,
+//     otherReducer
+// });
+
+// const middlewares = applyMiddleware(promiseMiddleWare())
+
+// const store = createStore(combined, middlewares)
+
+
+// export default store;
+
+
+
+
 // install redux and react-redux
-
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { promiseMiddleWare } from 'react-promise-middleware'
-
 //bring in the reducer(s)
+
+import { createStore } from 'redux';
+
 import reducer from './reducer';
 
-const combined = combineReducers({
-    userReducer,
-    otherReducer
-});
+export default createStore(reducer);
 
-const middlewares = applyMiddleware(promiseMiddleWare())
-
-const store = createStore(combined, middlewares)
-
-
-export default store;
